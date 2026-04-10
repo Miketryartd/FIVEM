850c8bc34f0c8611b70f22d26142de542b70218b
MikeTryArtd <mikeleano26@gmail.com>
2026-04-10 22:13:15 +0800

    make route for usercontroller

--- backend/Routes/UserRoutes.ts
+++ backend/Routes/UserRoutes.ts
@@ -1,4 +1,10 @@
-import Users from '../Models/Users.ts';
+import Users from '../Models/Users.js';
 import express from 'express';
+import { userRegister } from '../Controllers/UserController.js';
 const router = express.Router();
 
+
+router.post('/api/user/register', userRegister);
+
+
+export default router;
