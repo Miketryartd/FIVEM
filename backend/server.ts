53751d7c9e2dca6d929f765cabc2caf97eea3ae9
MikeTryArtd <mikeleano26@gmail.com>
2026-04-10 22:12:43 +0800

    added mongodb connection and use of routes

--- backend/server.ts
+++ backend/server.ts
@@ -1,11 +1,24 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
+import mongoose from "mongoose";
+import userRoute from "./Routes/UserRoutes.js";
 dotenv.config();
 
+//var
+var mongodb_uri = process.env.MONGODB_URI;
 
+//cors
 const app = express();
 app.use(cors());
+
+//mongoose
+mongoose.connect(mongodb_uri as string).then(() => console
+.log("Mongodb connected")).catch(err => console.log(err));
+
+
+//routes
+app.use('/fivem', userRoute);
 
 
 
